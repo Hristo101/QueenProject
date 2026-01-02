4542a698041b8c71334a195a2b9783d2e0761772
Hristo101 <92890172+Hristo101@users.noreply.github.com>
2026-01-02 13:07:54 +0200

    Implement 'save' command to persist game state
    - Added `saveGame` function using <fstream> to write game data to a text file.
    - The function serializes board dimensions, turn counter, and the matrix layout.
    - Integrated file saving logic into the main command loop.

--- UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn.cpp
+++ UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn.cpp
@@ -1,5 +1,5 @@
 ﻿
-
+#include <fstream>
 #include <iostream>
 using namespace std;
 const int MAX_ROW = 20;
@@ -21,7 +21,18 @@ bool areStringsEqual(const char* s1, const char* s2) {
         return false;
     }
 }
-
+void printHelp() {
+    cout << " new N M       -> Start new game (N x M)" << endl;
+    cout << " play x y      -> Place a queen at (x, y)" << endl;
+    cout << " free          -> List all valid moves" << endl;
+    cout << " show          -> Display the board" << endl;
+    cout << " save <file>   -> Save game to file" << endl;
+    cout << " load <file>   -> Load game from file" << endl;
+    cout << " back          -> Undo last move" << endl;
+    cout << " turn          -> Show who is next" << endl;
+    cout << " help          -> Show this menu" << endl;
+    cout << " exit          -> Exit the game" << endl;
+}
 void printMatrix(char matrix[MAX_ROW][MAX_COL], int row, int col) {
     for (int i = 0; i < row; i++)
     {
@@ -57,6 +68,22 @@ bool hasValidMoves(char matrix[MAX_ROW][MAX_COL],int row,int col) {
         }
     }
     return false;
+}
+void saveGame(char matrix[MAX_ROW][MAX_COL], int row, int col, int turnCounter, const char* filename) {
+    std::ofstream file(filename);
+
+    file << row << " " << col << " " << turnCounter << std::endl;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            file << matrix[i][j] << " ";
+        }
+        file << endl;
+    }
+
+    file.close();
 }
 void makeMove(char matrix[MAX_ROW][MAX_COL],int queenRow,int queenCol, int row, int col) {
     int index = 0;
@@ -103,6 +130,7 @@ int main()
     int row;
     int col;
     cin >> row >> col;
+    int turnCounter = 0;
     bool hasEmptySquare = true;
     char command[100];
     while (true)
@@ -112,38 +140,48 @@ int main()
         {
 
         }
-        if (areStringsEqual(command, "play"))
+        else if (areStringsEqual(command, "play"))
         {
 
         }
-        if (areStringsEqual(command, "free"))
+        else if (areStringsEqual(command, "free"))
         {
 
         }
-        if (areStringsEqual(command, "show"))
+        else if (areStringsEqual(command, "show"))
         {
 
         }
-        if (areStringsEqual(command, "save"))
+        else if (areStringsEqual(command, "save"))
         {
 
         }
-        if (areStringsEqual(command, "load"))
+        else if (areStringsEqual(command, "load"))
         {
 
         }
-        if (areStringsEqual(command, "turn"))
+        else if (areStringsEqual(command, "turn"))
+        {
+            if (turnCounter % 2 == 0)
+            {
+                cout << "Current turn: Player 1" << endl;
+            }
+            else {
+                cout << "Current turn: Player 2" << endl;
+            }
+        }
+        else if (areStringsEqual(command, "help"))
         {
 
         }
-        if (areStringsEqual(command, "help"))
+        else if (areStringsEqual(command,"exit"))
         {
+            return -1;
+        }
+        else if (areStringsEqual(command, "back")) {
 
         }
-        if (areStringsEqual(command,"exit"))
-        {
-
-        }
+       
     }
 }
 
