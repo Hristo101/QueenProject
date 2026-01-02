90e3e314022776ecdf448da3101e7b617bd57a87
Hristo101 <92890172+Hristo101@users.noreply.github.com>
2026-01-02 12:03:22 +0200

    Implement player move validation and board update logic
    - Added `placeQueen` function to handle move validation (bounds check, occupied cells, and attack zones).
    - Implemented `makeMove` to update the board state after a valid move.
    - Used direction arrays to efficiently mark attacked positions ('*') across rows, columns, and diagonals.

--- UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn.cpp
+++ UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn.cpp
@@ -32,6 +32,45 @@ void printMatrix(char matrix[MAX_ROW][MAX_COL], int row, int col) {
         cout << endl;
     }
 }
+void makeMove(char matrix[MAX_ROW][MAX_COL],int queenRow,int queenCol, int row, int col) {
+    int index = 0;
+    int rowChange[] = { 1,-1,0,0,-1,-1,1,1 };
+    int colChange[] = { 0,0,1,-1,1,-1,1,-1 };
+
+    for (int i = 0; i < 8; i++)
+    {
+        int currentRow = queenRow + rowChange[i];
+        int currentCol = queenCol + colChange[i];
+        while (currentRow >= 0 && currentRow < row && currentCol >=0 && currentCol < col)
+        {
+            if (matrix[currentRow][currentCol] == '.') {
+
+                matrix[currentRow][currentCol] = '*';
+            }
+
+            currentRow += rowChange[i];
+            currentCol += colChange[i];
+        }
+    }
+}
+
+void placeQueen(char matrix[MAX_ROW][MAX_COL],int row,int col, int r, int c, char player){
+
+    if (r < 0 || r >= row || c < 0 || c >= col) {
+        cout << "Error: Coordinates (" << r << "," << c << ") are out of bounds!" << endl;
+    }
+    else if (matrix[r][c] == '1' || matrix[r][c] == '2')
+    {
+        cout << "Error: Cell (" << r << "," << c << ") is already occupied!" << endl;
+    }
+    else if (matrix[r][c] == '*') {
+        cout << "Invalid move: This position is under attack!" << endl;
+    }
+    else {
+        matrix[r][c] = player;
+        makeMove(matrix, r, c,row,col);
+    }
+}
 int main()
 {
     std::cout << "Enter the matrix size\n";
