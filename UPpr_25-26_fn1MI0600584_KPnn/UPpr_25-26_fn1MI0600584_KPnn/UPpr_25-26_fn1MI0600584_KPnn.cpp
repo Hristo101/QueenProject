224772ea6ca091c0e526837f7b771d2ba9f78816
Hristo101 <92890172+Hristo101@users.noreply.github.com>
2026-01-02 12:14:13 +0200

    Implement 'free' command to list available moves
    - Added `printFreeCells` function to display coordinates of all valid empty spots.
    - The function iterates through the board and prints (row, col) for every '.' cell.
    - Connected the function to the "free" user command.

--- UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn.cpp
+++ UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn.cpp
@@ -32,6 +32,19 @@ void printMatrix(char matrix[MAX_ROW][MAX_COL], int row, int col) {
         cout << endl;
     }
 }
+void printFreeCells(char matrix[MAX_ROW][MAX_COL], int row, int col) {
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (matrix[i][j] == '.')
+            {
+              cout << matrix[i][j] << " "; 
+              cout << endl;
+            }
+        }
+    }
+}
 bool hasValidMoves(char matrix[MAX_ROW][MAX_COL],int row,int col) {
     for (int i = 0; i < row; i++)
     {
