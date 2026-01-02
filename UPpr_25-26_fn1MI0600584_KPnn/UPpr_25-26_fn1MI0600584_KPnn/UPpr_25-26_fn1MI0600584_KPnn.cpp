d29b6b64612ae7e437167087107de7dcbfe6d31f
Hristo101 <92890172+Hristo101@users.noreply.github.com>
2026-01-02 11:05:06 +0200

    Implement basic game loop and custom string comparison

---
+++ UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn/UPpr_25-26_fn1MI0600584_KPnn.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include <iostream>
+using namespace std;
+bool areStringsEqual(const char* s1, const char* s2) {
+    int i = 0;
+
+    while (s1[i] != '\0' && s2[i] != '\0') {
+
+        if (s1[i] != s2[i]) {
+            return false;
+        }
+        i++;
+    }
+    if (s1[i] == '\0' && s2[i] == '\0') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+int main()
+{
+    std::cout << "Enter the matrix size\n";
+    int row;
+    int col;
+    cin >> row >> col;
+    bool hasEmptySquare = true;
+    char command[100];
+    while (true)
+    {
+        cin >> command;
+        if (areStringsEqual(command,"play"))
+        {
+
+        }
+    }
+}
+
+
